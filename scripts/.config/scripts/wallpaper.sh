4d4d1bb9b0cf623011b54f5f8a5e1a83ce559a40
wade <sonic_371@yahoo.com.tw>
2026-02-04 16:26:59 +0800

    add systemd folder, update wallpaper script

--- scripts/.config/scripts/wallpaper.sh
+++ scripts/.config/scripts/wallpaper.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-PIDFILE="/tmp/wallpaper.pid"
-
-if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
-  echo "wallpaper.sh is already running."
-  exit
-fi
-
-echo $$ > "$PIDFILE"
-trap 'rm -f "$PIDFILE"' EXIT
-
-while true; do
-  # Using --randomize to select a random wallpaper
-  # Using --recursive to search for images in subdirectories
-  # Using --bg-fill to set the wallpaper
-  feh --recursive --randomize --bg-fill ~/Pictures/wallpaper/
-  # Change the sleep time to adjust the frequency of wallpaper changes
-  sleep 60 # 1 minutes
-done
+feh --recursive --randomize --bg-fill ~/Pictures/wallpaper/

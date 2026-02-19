5d9635c9708679f672ddc9acc719dcc919168759
wade <sonic_371@yahoo.com.tw>
2026-02-19 08:50:05 +0800

    update setup script

--- scripts/.config/scripts/setup.sh
+++ scripts/.config/scripts/setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check if script is run with sudo
-if [ "$EUID" -ne 0 ]; then
-    echo "This script requires sudo privileges to install packages and change shell."
-    echo "Please run it with: sudo $0"
-    exit 1
-fi
-
 # Exit on error
 set -e
 
@@ -16,35 +9,55 @@ set -x
 echo "Starting system setup..."
 
 # Update system first
-sudo pacman -Syu --noconfirm
+run_sudo pacman -Syu --noconfirm
 
 # Install core packages
-sudo pacman -S --noconfirm fd ripgrep fzf zoxide eza git stow yazi zsh
+run_sudo pacman -S --noconfirm fd ripgrep fzf zoxide eza git stow yazi zsh
 
 # Install base development tools
-sudo pacman -S --needed --noconfirm base-devel
+run_sudo pacman -S --needed --noconfirm base-devel
 
 # Install Xorg and related packages
-sudo pacman -S --noconfirm xorg-xinit xorg-server libxinerama libxft imlib2
+run_sudo pacman -S --noconfirm xorg-xinit xorg-server libxinerama libxft imlib2
 
 # Change default shell to zsh
 echo "Changing default shell to zsh..."
 chsh -s /usr/bin/zsh
 
-# Install Oh My Zsh (at the end)
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
 # Clone and stow dotfiles
 echo "Setting up dotfiles..."
-cd ~/dotfiles && stow -t ~ zshrc
+if [ ! -d "$HOME/dotfiles" ]; then
+    git clone https://github.com/sonic371/dotfiles.git $HOME/dotfiles
+else
+    echo "Dotfiles directory already exists, skipping clone..."
+fi
+
+# Run stow with --override flag to handle existing files
+cd $HOME/dotfiles && stow --verbose --override=.* -t $HOME zshrc
 
 # Clone additional repositories
 echo "Cloning additional repositories..."
-git clone https://github.com/bakkeby/st-flexipatch.git ~/.local/src/st-flexipatch
-git clone https://github.com/sonic371/dwm ~/.local/src/dwm
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+mkdir -p $HOME/.local/src
+
+if [ ! -d "$HOME/.local/src/st-flexipatch" ]; then
+    git clone https://github.com/bakkeby/st-flexipatch.git $HOME/.local/src/st-flexipatch
+else
+    echo "st-flexipatch directory already exists, skipping clone..."
+fi
+
+if [ ! -d "$HOME/.local/src/dwm" ]; then
+    git clone https://github.com/sonic371/dwm $HOME/.local/src/dwm
+else
+    echo "dwm directory already exists, skipping clone..."
+fi
+
+# Install Oh My Zsh (only if not already installed)
+echo "Installing Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh already installed, skipping..."
+fi
 
 echo "Setup complete!"
 echo "NOTE: You may need to log out and back in for shell changes to take effect."

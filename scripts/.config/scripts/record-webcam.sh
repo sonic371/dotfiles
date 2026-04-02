a41740c35350f322dbe6c3d63fe05934c2b81605
wade <sonic_371@yahoo.com.tw>
2026-04-03 02:24:24 +0800

    Add back wallpaper startup, update webcam script

--- scripts/.config/scripts/record-webcam.sh
+++ scripts/.config/scripts/record-webcam.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-ffplay -video_size 640x480 -window_title "Webcam" -i /dev/video0 >/dev/null 2>&1 &
+# ffplay -video_size 640x480 -window_title "Webcam" -i /dev/video0 >/dev/null 2>&1 &
+mpv --no-osc --osd-level=0 --no-border --geometry=640x480 --title="Webcam" --x11-name=Webcam av://v4l2:/dev/video0 >/dev/null 2>&1 &
